89b8164e085c5deb245f0ddecb9209bece6314cd
danfedick <daniel.fedick@hashicorp.com>
2021-12-21 13:20:06 -0500

    RM the vars and providers

--- providers.tf
+++ providers.tf
@@ -9,9 +9,6 @@ terraform {
 
 provider "aws" {
   region     = var.region
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-  token      = var.AWS_SESSION_TOKEN
 
   default_tags {
     tags = {
--- variables.tf
+++ variables.tf
@@ -3,22 +3,3 @@ variable "region" {
   description = "AWS Region"
   default     = "us-east-2"
 }
-variable "AWS_ACCESS_KEY_ID" {
-  type        = string
-  description = "This is the value of AWS_ACCESS_KEY_ID"
-  default     = "x---"
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  type        = string
-  description = "This is the value of AWS_SECRET_ACCESS_KEY"
-  default     = "x---"
-  sensitive   = true
-}
-
-variable "AWS_SESSION_TOKEN" {
-  type        = string
-  description = "This is the value of AWS_SESSION_TOKEN"
-  default     = "x---"
-  sensitive   = true
-}
