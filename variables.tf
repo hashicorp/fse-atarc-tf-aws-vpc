b538ad1ac0210ad4df4d4f82570c798ff277199c
danfedick <daniel.fedick@hashicorp.com>
2021-12-21 13:03:09 -0500

    Modify AWS vars - Add Session token

--- variables.tf
+++ variables.tf
@@ -3,15 +3,22 @@ variable "region" {
   description = "AWS Region"
   default     = "us-east-2"
 }
-variable "access_key" {
+variable "AWS_ACCESS_KEY_ID" {
   type        = string
   description = "This is the value of AWS_ACCESS_KEY_ID"
   default     = "x---"
 }
 
-variable "secret_key" {
+variable "AWS_SECRET_ACCESS_KEY" {
   type        = string
   description = "This is the value of AWS_SECRET_ACCESS_KEY"
+  default     = "x---"
+  sensitive   = true
+}
+
+variable "AWS_SESSION_TOKEN" {
+  type        = string
+  description = "This is the value of AWS_SESSION_TOKEN"
   default     = "x---"
   sensitive   = true
 }
